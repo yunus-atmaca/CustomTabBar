07937c305e8ad7d691165805508c9b1b73339270
yunus-atmaca <yunusatmacabm@gmail.com>
2022-06-26 15:25:31 +0300

    init

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView style={{flex:1, backgroundColor:'blue'}}>
+
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
