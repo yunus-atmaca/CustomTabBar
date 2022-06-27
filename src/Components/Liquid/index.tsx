f4ce3c11f2da5b03f48e3502eaaac0dcad5cf7c4
yunus-atmaca <yunusatmacabm@gmail.com>
2022-06-27 15:25:38 +0300

    add plus anim

--- src/Components/Liquid/index.tsx
+++ src/Components/Liquid/index.tsx
@@ -8,25 +8,38 @@ import Animated, {
   withTiming,
   Easing,
   runOnJS,
+  useAnimatedStyle,
+  withDelay,
 } from 'react-native-reanimated';
 
 import {Constants} from '../../utils';
+
+const AnimatedPath = Animated.createAnimatedComponent(Path);
 
 //unit vertical
 const V = 12;
 //unit horizontal
 const H = (Constants.S_WIDTH - 48) / 20;
+//unitDuration
+const U_D = 100;
 
 const TAB_HEIGHT_F = 12 * 6;
-
-const AnimatedPath = Animated.createAnimatedComponent(Path);
+const TAB_WIDTH_F = Constants.S_WIDTH - 48;
 
 const Liquid: FC<BottomTabBarProps> = ({}) => {
   const mul = useRef(1);
 
-  const Q1 = useSharedValue(1);
-  const Q2 = useSharedValue(2);
-  const Q3 = useSharedValue(3);
+  const plusY = useSharedValue(0);
+  const width = useSharedValue((TAB_HEIGHT_F / 4) * 3);
+  const animatedPlus = useAnimatedStyle(() => {
+    return {
+      transform: [{translateY: plusY.value}],
+      width: width.value,
+    };
+  });
+
+  const Q1 = useSharedValue(2);
+  const Q2 = useSharedValue(3);
 
   const animatedProps = useAnimatedProps(() => {
     const path = `
@@ -39,11 +52,13 @@ const Liquid: FC<BottomTabBarProps> = ({}) => {
     V ${2 * V}
     Q ${20 * H} 0 ${18 * H} 0
     H ${16 * H}
-    Q ${15 * H} ${0 * V} ${14 * H} ${Q1.value * V}
-    C ${13 * H} ${Q2.value * V} ${11 * H} ${Q3.value * V} ${10 * H} ${
-      Q3.value * V
+    Q ${15 * H} ${0 * V} ${14 * H} ${(Q1.value / 2) * V}
+    C ${13 * H} ${Q1.value * V} ${11 * H} ${Q2.value * V} ${10 * H} ${
+      Q2.value * V
     }
-    C ${9 * H} ${Q3.value * V} ${7 * H} ${Q2.value * V} ${6 * H} ${Q1.value * V}
+    C ${9 * H} ${Q2.value * V} ${7 * H} ${Q1.value * V} ${6 * H} ${
+      (Q1.value / 2) * V
+    }
     Q ${5 * H} ${0 * V} ${4 * H} ${0 * V}
     H ${2 * H}
   `;
@@ -53,52 +68,77 @@ const Liquid: FC<BottomTabBarProps> = ({}) => {
   });
 
   useEffect(() => {
-    Q1.value = withTiming(0, {
-      duration: 300,
-      easing: Easing.linear,
-    });
+    animationPath();
+    animationPlusY();
+    return () => {
+      Q1.value = 2;
+      Q2.value = 3;
 
-    Q2.value = withTiming(0, {
-      duration: 300,
-      easing: Easing.linear,
-    });
-
-    const q3Finished = (oldVal: number) => {
-      if (oldVal === 0) return;
-
-      const toVal = getNewValue(oldVal);
-
-      Q3.value = withTiming(
-        toVal,
-        {
-          duration: 150,
-          easing: Easing.ease,
-        },
-        () => {
-          'worklet';
-          runOnJS(q3Finished)(toVal);
-        },
-      );
+      plusY.value = 0;
+      width.value = (TAB_HEIGHT_F / 4) * 3;
     };
+  }, []);
 
-    Q3.value = withTiming(
-      -2,
+  const animationPlusY = () => {
+    plusY.value = withTiming(
+      -4 * V,
       {
-        duration: 300,
+        duration: U_D * 1.5,
         easing: Easing.linear,
       },
       () => {
         'worklet';
-        runOnJS(q3Finished)(-2);
+        runOnJS(animPlusW)();
       },
     );
+  };
 
-    return () => {
-      Q1.value = 1;
-      Q2.value = 2;
-      Q3.value = 3;
+  const animPlusW = () => {
+    width.value = withDelay(
+      75,
+      withTiming((TAB_WIDTH_F / 4) * 3, {
+        duration: U_D * 2.5,
+        easing: Easing.linear,
+      }),
+    );
+  };
+
+  const animationPath = () => {
+    Q1.value = withTiming(0, {
+      duration: U_D * 2.5,
+      easing: Easing.linear,
+    });
+
+    const q2Finished = (oldVal: number) => {
+      if (oldVal === 0) return;
+
+      const toVal = getNewValue(oldVal);
+
+      Q2.value = withTiming(
+        toVal,
+        {
+          duration: U_D * toVal,
+          easing: Easing.ease,
+        },
+        () => {
+          'worklet';
+          runOnJS(q2Finished)(toVal);
+        },
+      );
     };
-  }, []);
+
+    Q2.value = withTiming(
+      -2,
+      {
+        duration: U_D * 2.5,
+        easing: Easing.linear,
+      },
+      () => {
+        'worklet';
+        runOnJS(q2Finished)(-2);
+      },
+    );
+  };
 
   const getNewValue = (currentVal: number) => {
     const absVal = Math.abs(currentVal);
@@ -110,7 +150,7 @@ const Liquid: FC<BottomTabBarProps> = ({}) => {
     }
 
     mul.current *= -1;
-    console.debug('returnVal: ', returnVal);
+    //console.debug('returnVal: ', returnVal);
     return returnVal;
   };
 
@@ -119,12 +159,15 @@ const Liquid: FC<BottomTabBarProps> = ({}) => {
       <View style={styles.svgContainer}>
         {
           <Svg
-            viewBox={`0 -40 ${Constants.S_WIDTH - 48} ${TAB_HEIGHT_F * 2}`}
+            viewBox={`0 
+              -${TAB_HEIGHT_F / 2} 
+              ${Constants.S_WIDTH - 48} 
+              ${TAB_HEIGHT_F + (TAB_HEIGHT_F / 4) * 3}
+              `}
             style={{
               width: '100%',
-              height: TAB_HEIGHT_F + 100,
-              backgroundColor: 'gray',
-              paddingTop: 50,
+              height: TAB_HEIGHT_F + (TAB_HEIGHT_F / 4) * 3,
+              //backgroundColor: 'gray',
             }}>
             <AnimatedPath
               animatedProps={animatedProps}
@@ -136,6 +179,9 @@ const Liquid: FC<BottomTabBarProps> = ({}) => {
           </Svg>
         }
       </View>
+      <View style={styles.plusContainer}>
+        <Animated.View style={[styles.plus, animatedPlus]}></Animated.View>
+      </View>
     </View>
   );
 };
@@ -146,17 +192,30 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    //height: TAB_HEIGHT_B,
     width: Constants.S_WIDTH,
-    backgroundColor: 'red',
+    //backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
     paddingHorizontal: 24,
   },
   svgContainer: {
-    //height: TAB_HEIGHT_F,
     width: '100%',
-    overflow: 'visible',
+    //backgroundColor: 'green',
+  },
+  plusContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: TAB_HEIGHT_F,
+    //backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  plus: {
+    height: (TAB_HEIGHT_F / 4) * 3,
+    borderRadius: TAB_HEIGHT_F,
+    backgroundColor: '#4C1345',
   },
 });
 
